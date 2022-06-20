993233ce70281fa37883a9abad015a30f582288c
revanzli <revanzekeryeli2002@gmail.com>
2022-06-20 21:10:43 +0400

    feat: added LikeType Entites

---
+++ Aniverse/src/blog-service/core/BlogService.Domain/Entities/LikeType.cs
@@ -0,0 +1,15 @@
+﻿using BlogService.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogService.Domain.Entities
+{
+    public class LikeType : BaseEntity
+    {
+        public string Name { get; set; }
+        public string IconSrc { get; set; }
+    }
+}
