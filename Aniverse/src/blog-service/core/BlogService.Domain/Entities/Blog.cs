566491d9d6505d86bd63fe47b0d7edf4c0d16b77
revanzli <revanzekeryeli2002@gmail.com>
2022-06-20 21:07:38 +0400

    feat: added comment and blog

--- Aniverse/src/blog-service/core/BlogService.Domain/Entities/Blog.cs
+++ Aniverse/src/blog-service/core/BlogService.Domain/Entities/Blog.cs
@@ -4,5 +4,11 @@ namespace BlogService.Domain.Entities
 {
     public class Blog : BaseEntity
     {
+        public string UserId { get; set; }
+        public string Descreption { get; set; }
+        public ICollection<string> Hastags { get; set; }
+        public ICollection<string> Images { get; set; }
+        public ICollection<Like> Likes { get; set; }
+        public ICollection<Comment> Comments { get; set; }
     }
 }
--- Aniverse/src/blog-service/core/BlogService.Domain/Entities/Comment.cs
+++ Aniverse/src/blog-service/core/BlogService.Domain/Entities/Comment.cs
@@ -4,5 +4,13 @@ namespace BlogService.Domain.Entities
 {
     public class Comment : BaseEntity
     {
+        public string UserId { get; set; }
+        public string BlogId { get; set; } 
+        public string Descreption { get; set; }
+        public string CommentId { get; set; }
+        public ICollection<Comment> ReplyComments { get; set; }
+        public ICollection<string> Images { get; set; }
+        public ICollection<string> Hastags { get; set; }
+        public ICollection<Like> Likes { get; set; }
     }
 }
