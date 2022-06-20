61d525abf751723ca3806fe249c7a28b4acfe411
revanzli <revanzekeryeli2002@gmail.com>
2022-06-20 10:01:00 +0400

    added refresh token

---
+++ Aniverse/src/auth-service/AuthService.API/DTO_s/Token/TokenModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthService.API.DTO_s.TOken
+{
+    public class TokenModel
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
--- Aniverse/src/auth-service/AuthService.API/Service/Implementations/AuthService.cs
+++ Aniverse/src/auth-service/AuthService.API/Service/Implementations/AuthService.cs
@@ -1,5 +1,6 @@
 ﻿using AuthService.API.DataAccessLayer.Entites;
 using AuthService.API.DTO_s.Login;
+using AuthService.API.DTO_s.TOken;
 using AuthService.API.Service.Abstractions;
 using Microsoft.AspNetCore.Identity;
 using System.IdentityModel.Tokens.Jwt;
@@ -13,7 +14,9 @@ namespace AuthService.API.Service.Implementations
         private readonly ITokenService _tokenService;
         private readonly IConfiguration _config;
 
-        public AuthService(UserManager<AppUser> userManager, ITokenService tokenService, IConfiguration config)
+        public AuthService(UserManager<AppUser> userManager,
+                           ITokenService tokenService,
+                           IConfiguration config)
         {
             _userManager = userManager;
             _tokenService = tokenService;
@@ -26,12 +29,12 @@ namespace AuthService.API.Service.Implementations
             if (user is null) throw new Exception("User not found");
             if (!await _userManager.CheckPasswordAsync(user, login.Password)) throw new UnauthorizedAccessException();
             var roles = _userManager.GetRolesAsync(user).Result;
-            var authClaims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.NameIdentifier, user.Id),
-                    new Claim(ClaimTypes.Name, user.UserName),
-                    new Claim(ClaimTypes.Email, user.Email),
-                };
+            List<Claim> authClaims = new()
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email),
+            };
             authClaims.AddRange(roles.Select(userRole => new Claim(ClaimTypes.Role, userRole)));
             var token = _tokenService.CreateToken(authClaims);
             var refreshToken = _tokenService.GenerateRefreshToken();
@@ -44,6 +47,29 @@ namespace AuthService.API.Service.Implementations
                 Token = new JwtSecurityTokenHandler().WriteToken(token),
                 RefreshToken = refreshToken,
                 Expiration = token.ValidTo
+            };
+        }
+        public async Task<TokenModel> RefreshToken(TokenModel tokenModel)
+        {
+            if (tokenModel is null) throw new NullReferenceException(nameof(tokenModel));
+            string accessToken = tokenModel.AccessToken;
+            string refreshToken = tokenModel.RefreshToken;
+            var principal = _tokenService.GetPrincipalFromExpiredToken(accessToken);
+            if (principal == null) throw new UnauthorizedAccessException();
+            string username = principal.Identity.Name;
+            var user = await _userManager.FindByNameAsync(username);
+            if (user == null || user.RefreshToken != refreshToken || user.RefreshTokenExpiryTime <= DateTime.Now)
+                throw new NullReferenceException();
+            var newAccessToken = _tokenService.CreateToken(principal.Claims.ToList());
+            var newRefreshToken = _tokenService.GenerateRefreshToken();
+
+            user.RefreshToken = newRefreshToken;
+            await _userManager.UpdateAsync(user);
+
+            return new TokenModel
+            {
+                AccessToken = new JwtSecurityTokenHandler().WriteToken(newAccessToken),
+                RefreshToken = newRefreshToken
             };
         }
     }
