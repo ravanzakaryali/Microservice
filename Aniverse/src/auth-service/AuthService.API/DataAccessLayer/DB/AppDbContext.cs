61b4c28be94bfd6df310a61bcc30a3a279bcc207
revanzli <revanzekeryeli2002@gmail.com>
2022-06-20 08:53:28 +0400

    feat: added AppDbContext

---
+++ Aniverse/src/auth-service/AuthService.API/DataAccessLayer/DB/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.API.DataAccessLayer.DB
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<DbContext> options) : base(options) { }
+    }
+}
