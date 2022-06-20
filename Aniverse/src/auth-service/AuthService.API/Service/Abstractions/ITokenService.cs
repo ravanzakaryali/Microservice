2bf66f8214250348f1721c7ab64991690ddbe0a3
revanzli <revanzekeryeli2002@gmail.com>
2022-06-20 08:57:44 +0400

    feat: added Token service

---
+++ Aniverse/src/auth-service/AuthService.API/Service/Abstractions/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace AuthService.API.Service.Abstractions
+{
+    public interface ITokenService
+    {
+        public JwtSecurityToken CreateToken(List<Claim> authClaims);
+    }
+}
---
+++ Aniverse/src/auth-service/AuthService.API/Service/Implementations/TokenService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthService.API.Service.Implementations
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _config;
+
+        public TokenService(IConfiguration config)
+        {
+            _config = config;
+        }
+        public JwtSecurityToken CreateToken(List<Claim> authClaims)
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:securityKey"]));
+            _ = int.TryParse(_config["JWT:TokenValidityInMinutes"], out int tokenValidityInMinutes);
+
+            var token = new JwtSecurityToken(
+                issuer: _config["JWT:issuer"],
+                audience: _config["JWT:audience"],
+                expires: DateTime.Now.AddMinutes(tokenValidityInMinutes),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+
+            return token;
+        }
+    }
+}
