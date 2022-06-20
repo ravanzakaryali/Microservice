d9fa40988b947a87f0816240adb7a8b49e50b518
revanzli <revanzekeryeli2002@gmail.com>
2022-06-20 08:50:38 +0400

    feat: added AppUser entity

---
+++ Aniverse/src/auth-service/AuthService.API/DataAccessLayer/Entites/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthService.API.DataAccessLayer.Entites
+{
+    public class AppUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
