1a57ed4569c2cb57194bd417b82388b681470c66
revanzli <revanzekeryeli2002@gmail.com>
2022-06-21 17:59:25 +0400

    feat: added blog configuration

--- Aniverse/src/post-service/Core/PostService.Domain/Entities/Post.cs
+++ Aniverse/src/post-service/Core/PostService.Domain/Entities/Post.cs
@@ -5,7 +5,7 @@ namespace PostService.Domain.Entities
     public class Post : BaseEntity
     {
         public Guid UserId { get; set; }
-        public string Descreption { get; set; }
+        public string Content { get; set; }
         public ICollection<Like> Likes { get; set; }
         public ICollection<Comment> Comments { get; set; }
         public ICollection<Share> Shares { get; set; }
---
+++ Aniverse/src/post-service/Infrastructure/PostService.Persistence/Configuration/BlogConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PostService.Domain.Entities;
+
+namespace PostService.Persistence.Configuration
+{
+    public class BlogConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.Property(b => b.IsDeleted).HasDefaultValue(false);
+            builder.Property(b => b.CreatedDate).HasDefaultValueSql("GETDATE()");
+            builder.Property(b => b.Content).HasMaxLength(369);
+        }
+    }
+}
