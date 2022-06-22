326076cc56f8939ac17a3219a6b368456b2de1c0
revanzli <revanzekeryeli2002@gmail.com>
2022-06-22 15:23:04 +0400

    feat: added repository in BlogService

---
+++ Aniverse/src/post-service/Core/PostService.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostService.Domain.Common;
+using System.Linq.Expressions;
+
+namespace PostService.Domain.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        DbSet<T> Table { get; }
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> exp = null, 
+                                  bool tracking = true,
+                                  params string[] includes);
+        Task<List<T>> GetAllAsync<TOrderBy>(int page,
+                                            int size,
+                                            Expression<Func<T, TOrderBy>> orderBy, 
+                                            Expression<Func<T, bool>> exp = null, 
+                                            bool tracking = true,
+                                            params string[] includes);
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate = null, 
+                        bool tracking = true, 
+                        params string[] include);
+        Task<T> GetAsync(string id, 
+                         bool tracking = true, 
+                         params string[] include);
+        Task<T> AddAsync(T entity);
+        Task<List<T>> AddRangeAsync(List<T> listModel);
+        T Remove(T entity);
+        List<T> RemoveRange(List<T> listModel);
+        Task<T> RemoveAsync(string id);
+        T Update(T entity);
+        Task<int> SaveAsync();
+    }
+}
