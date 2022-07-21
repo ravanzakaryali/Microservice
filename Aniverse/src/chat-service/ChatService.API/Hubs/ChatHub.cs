8fcbb6ffb796be9319530f1a497a7f55485eb86e
revanzli <revanzekeryeli2002@gmail.com>
2022-07-21 20:35:04 +0400

    feat: added SendMessage mehtod

--- Aniverse/src/chat-service/ChatService.API/Hubs/ChatHub.cs
+++ Aniverse/src/chat-service/ChatService.API/Hubs/ChatHub.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using ChatService.API.Service.Interfaces;
+using Microsoft.AspNetCore.SignalR;
 
 namespace ChatService.API.Hubs
 {
     public class ChatHub : Hub
     {
+        private readonly IRabbitMqService _mqService;
+        public async Task SendMessage(string userId,string message)
+        {
+            await Clients.User(userId).SendAsync("receiveMessage", message);
+        }
     }
 }
--- Aniverse/src/chat-service/ChatService.API/Program.cs
+++ Aniverse/src/chat-service/ChatService.API/Program.cs
@@ -4,8 +4,10 @@ using ChatService.API.Service.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.Configure<RabbitMqConfiguration>(a => builder.Configuration.GetSection(nameof(RabbitMqConfiguration)).Bind(a));
+builder.Services.Configure<RabbitMqConfiguration>(a => 
+builder.Configuration.GetSection(nameof(RabbitMqConfiguration)).Bind(a));
 builder.Services.AddSingleton<IRabbitMqService, RabbitMqService>();
+builder.Services.AddSignalR();
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.Run();
