76b59221024e2a4465556ec2ce71eedf3a002c97
revanzli <revanzekeryeli2002@gmail.com>
2022-07-23 01:08:11 +0400

    refactor: remove rabbitMqService

--- Aniverse/src/chat-service/ChatService.API/Hubs/ChatHub.cs
+++ Aniverse/src/chat-service/ChatService.API/Hubs/ChatHub.cs
@@ -1,14 +1,20 @@
 ﻿using ChatService.API.Service.Interfaces;
 using Microsoft.AspNetCore.SignalR;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace ChatService.API.Hubs
 {
     public class ChatHub : Hub
     {
         private readonly IRabbitMqService _mqService;
-        public async Task SendMessage(string userId,string message)
+        public async Task SendMessage(string userId, string message)
         {
             await Clients.User(userId).SendAsync("receiveMessage", message);
+        }
+        public async Task SendMessageAll(string message)
+        {
+            await Clients.All.SendAsync(message);
         }
     }
 }
--- Aniverse/src/chat-service/ChatService.API/Program.cs
+++ Aniverse/src/chat-service/ChatService.API/Program.cs
@@ -1,12 +1,7 @@
 using ChatService.API.DTOs;
-using ChatService.API.Service.Implementations;
-using ChatService.API.Service.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.Configure<RabbitMqConfiguration>(a => 
-builder.Configuration.GetSection(nameof(RabbitMqConfiguration)).Bind(a));
-builder.Services.AddSingleton<IRabbitMqService, RabbitMqService>();
 builder.Services.AddSignalR();
 var app = builder.Build();
 app.UseHttpsRedirection();
