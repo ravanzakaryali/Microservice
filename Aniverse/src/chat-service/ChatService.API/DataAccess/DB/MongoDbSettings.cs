97124368296cf77ab74c4b151145e2a98200fa81
revanzli <revanzekeryeli2002@gmail.com>
2022-07-23 21:18:46 +0400

    feat: added Message entities

---
+++ Aniverse/src/chat-service/ChatService.API/DataAccess/DB/MongoDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatService.API.DataAccess.DB
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionURI { get; set; }
+        public string DatabaseName { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
---
+++ Aniverse/src/chat-service/ChatService.API/DataAccess/Entities/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatService.API.DataAccess.Entities
+{
+    public class Message
+    {
+        public string Id { get; set; }
+        public string Content { get; set; }
+        public DateTime SenderDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public string SenderUserId { get; set; }
+        public string ReceiverUserId { get; set; }
+    }
+}
